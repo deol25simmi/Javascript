671ddb961a4753d0b1621350cff48c850f718958
deol25simmi <105154354+deol25simmi@users.noreply.github.com>
2023-10-19 12:32:05 +0000

    numbers and maths in js

---
+++ 01_basics/06_numbers.js
@@ -0,0 +1,31 @@
+const n = 400
+//console.log(n)
+
+const num = new Number(300)
+//console.log(num)
+
+// console.log(num.toString())
+// console.log(num.toFixed(2))
+
+const x = 43.12
+//console.log(x.toPrecision(3))
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+/*****************************MATHS******* */
+// console.log(Math.PI * 12)
+// console.log(Math.abs(-7));
+// console.log(Math.round(6.7));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(0,-1,2,4));
+//console.log(Math.random()); =>between 0 & 1
+//console.log(Math.floor(Math.random()*10)  +1);
+const max = 10
+const min = 6
+
+// formula  impp**** 
+//Math.floor(Math.random()* (max-min+1)) +min
