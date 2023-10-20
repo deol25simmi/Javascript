bb706004b41af27ca12d30bcf844f8057eaa869b
deol25simmi <105154354+deol25simmi@users.noreply.github.com>
2023-10-20 11:53:55 +0000

    arrays methods

---
+++ 02_arrays/01_array.js
@@ -0,0 +1,39 @@
+let arr = [1,2,3,'sam','deol']
+//console.log(arr[0])
+
+const arry  = new Array(0,3,'ds','ssa')
+//console.log(arry[3])
+
+//array nethods
+
+// arry.push(5)
+// console.log(arry);
+// arry.push('hhi')
+// Array.pop()
+
+arry.unshift(9)
+arry.shift()
+// console.log(arry)
+// console.log(arry.includes('hi'));
+// console.log(arry.indexOf(9));
+
+// const newS = arry.join()
+// console.log(typeof arry); = obejct
+// console.log(typeof newS)= string
+
+//slice    splice
+
+//console.log("a",arry)
+// const myn1 =  arry.slice(1,3)
+// console.log("a",myn1);
+// console.log(arry)
+// const myn2 = arry.splice(1,3)
+// console.log("b",myn2)
+// console.log("c",arry)
+
+
+
+let a = [1,2,3,"sam"]
+let b = ["dsoi",901,01,129,"as"]
+let c = a.concat(b)
+console.log(c);
